844ca47b9eef4222ba1fcc345f8ba60a55cb7cfc
m2构造机 <zhanggaoqi@aobi.com>
2026-07-07 14:46:15 +0800

    [Branch Release] v1.1.7-dev-1480088 - develop
    - 更新所有 XCFrameworks 到 1.1.7-dev-1480088
    - 更新资源文件
    - 更新 checksums
    - 分支测试版本（无 tag）

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
         .target(
             name: "BTMultiSplashWrapper",
             dependencies: [
+                .byName(name: "BTMultiSplash"),
                 .product(name: "BTSDKUIKitCore", package: "BTSDKUIKitCore"),
             ],
             path: "BTMultiSplashWrapper",
@@ -30,5 +31,10 @@ let package = Package(
         ),
         
         // ========== Binary Frameworks ==========
+        .binaryTarget(
+            name: "BTMultiSplash",
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/BTMultiSplash/1.1.7-dev-1480088/BTMultiSplash.xcframework.zip",
+            checksum: "0b5188d453520c6464a0cfc5eb14be6fe097f1b518c569a66a746d3bb7788edd"
+        )
     ]
 )
